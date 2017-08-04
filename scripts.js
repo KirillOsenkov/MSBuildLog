6b9763e3382b75c7da8a3f480d8b575f6aa18991
Kirill Osenkov <github@osenkov.com>
2017-08-04 13:44:30 -0700

    Start adding the content.

---
+++ scripts.js
@@ -0,0 +1,16 @@
+﻿function getUrl(url, callback) {
+    //return $.get(url, null, callback, null);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.setRequestHeader("Accept", "text/html");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            var data = xhr.responseText;
+            if (typeof data === "string" && data.length > 0) {
+                callback(data);
+            }
+        }
+    };
+    xhr.send();
+    return xhr;
+}
